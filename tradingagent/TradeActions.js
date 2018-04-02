83d006a015e307d2bc9368d30773c083f435aa0c
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2018-04-01 20:36:28 -0700

    wip running the agent on gdax test environment

--- tradingagent/TradeActions.js
+++ tradingagent/TradeActions.js
@@ -11,3 +11,45 @@ const authedClient = new Gdax.AuthenticatedClient(
   passphrase,
   apiURI: sandboxURI
 );
+
+
+// get accounts
+
+//
+
+// Buy 1 BTC @ 100 USD
+// const buyParams = {
+//   price: '100.00', // USD
+//   size: '1', // BTC
+//   product_id: 'BTC-USD',
+// };
+// authedClient.buy(buyParams, callback);
+
+// // Sell 1 BTC @ 110 USD
+// const sellParams = {
+//   price: '110.00', // USD
+//   size: '1', // BTC
+//   product_id: 'BTC-USD',
+// };
+// authedClient.sell(sellParams, callback);
+
+
+// authedClient.cancelOrders(callback);
+
+// authedClient.getFills(callback);
+// // For pagination, you can include extra page arguments
+// authedClient.getFills({ before: 3000 }, callback);
+
+
+// authedClient.getAccountHolds(accountID, callback);
+
+// // For pagination, you can include extra page arguments
+// authedClient.getAccountHolds(accountID, { before: 3000 }, callback);
+
+
+module.exports = {
+  buy,
+  sell,
+  getFills,
+  getAccountHolds
+}
