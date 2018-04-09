c54e8ef0cc05553889eb7c53a9634881d553058c
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2018-04-09 15:34:00 -0700

    installed 0x

---
+++ datafeed/ZeroEx/index.js
@@ -0,0 +1,12 @@
+const ZeroEx = require('0x.js');
+
+// async function getAddresses() {
+//   try {
+//       var availableAddresses = await zeroEx.getAvailableAddressesAsync();
+//       console.log(availableAddresses)
+//   } catch (error) {
+//       console.log('Caught error: ', error);
+//   }
+// }
+
+// getAddresses();
